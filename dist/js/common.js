12a70823d809b77bc8aa5db439afe537cc3f0326
= <=>
2018-06-25 18:58:09 +0300

    finished

--- dist/js/common.js
+++ dist/js/common.js
@@ -69,5 +69,70 @@
        of last tab */
     $('#tabs-container2 ul.tabs li').last().addClass("tab_last2");
 
+    /**********************************************************/
+
+    $('.more__link').on("click", function(e) {
+        e.preventDefault();
+        $(this).fadeOut();
+        $(this).parents('.more ').prev().find('.gallery__images-more').css("opacity", "1").css("display", "flex");
+    });
+
+    /*menu*/
+    let burger = document.querySelector('.burger');
+    let menu = document.querySelector('.adaptive-nav');
+
+    window.addEventListener('click', function(e){
+        if (burger.contains(e.target)){
+            // Clicked in box
+            burger.classList.add('active');
+            burger.querySelector('.glyphicon').classList.remove('glyphicon-align-justify');
+            burger.querySelector('.glyphicon').classList.add('glyphicon-remove');
+            burger.nextElementSibling.classList.add('adaptive-nav__open');
+            document.querySelector('.header__container').style.zIndex = 12;
+            document.body.style.overflowY = 'hidden';
+        } else{
+            // Clicked outside the box
+            burger.classList.remove('active');
+            burger.querySelector('.glyphicon').classList.remove('glyphicon-remove');
+            burger.querySelector('.glyphicon').classList.add('glyphicon-align-justify');
+            burger.nextElementSibling.classList.remove('adaptive-nav__open');
+            document.body.style.overflowY = 'scroll';
+        }
+    });
+    /*menu*/
+
+    // nav's anchor
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            // On-page links
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function() {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        }
+                    });
+                }
+            }
+        });
+    // nav's anchor
 
 })();
