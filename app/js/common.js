1d07908d21ec93e03293c46b2f53319980e83426
= <=>
2018-07-04 17:52:11 +0300

    finished desctop

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,16 @@
 (function (){
-    
+    const headerMenu = document.querySelector('.header__menu');
+
+    headerMenu.addEventListener('click', function() {
+        if (this.querySelector('.burger').classList.contains('burger__open')) {
+            this.querySelector('.burger').classList.remove('burger__open');
+            this.querySelector('.navigation').classList.remove('navigation__open');
+            this.querySelector('.burger__image').setAttribute('src', 'img/burger.png');
+        } else {
+            this.querySelector('.burger').classList.add('burger__open');
+            this.querySelector('.navigation').classList.add('navigation__open');
+            this.querySelector('.burger__image').setAttribute('src', 'img/close.png');
+        }
+    });
 
 })();
